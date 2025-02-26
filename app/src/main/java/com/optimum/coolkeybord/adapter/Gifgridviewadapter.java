6d4730077c5a325b5b3f59f9e44d0fecd746a26d
suyash-modi <134408224+suyash-modi@users.noreply.github.com>
2025-02-26 14:38:43 +0530

    add feature show text instead of thumbnail

--- app/src/main/java/com/optimum/coolkeybord/adapter/Gifgridviewadapter.java
+++ app/src/main/java/com/optimum/coolkeybord/adapter/Gifgridviewadapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,29 +21,32 @@ import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.optimum.coolkeybord.R;
 import com.optimum.coolkeybord.models.Gifdata;
-
+import com.optimum.coolkeybord.settingssession.SettingSesson;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Gifgridviewadapter extends   RecyclerView.Adapter<Gifgridviewadapter.ViewHolder> {
     private final ArrayList<Gifdata> mDataset;
     private final Context context;
-
+    private final SettingSesson session;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public ImageView gifitemgg;
         public Button button;
+        public TextView gifText;
 
         public ViewHolder(View v) {
             super(v);
             gifitemgg = v.findViewById(R.id.gifitemgg);
+            gifText = v.findViewById(R.id.multiline_text);
         }
     }
 
     public Gifgridviewadapter(ArrayList<Gifdata> myDataset, Context context) {
         mDataset = myDataset;
         this.context = context;
-
+        this.session = new SettingSesson(context);
     }
 
     @NonNull
@@ -55,22 +59,34 @@ public class Gifgridviewadapter extends   RecyclerView.Adapter<Gifgridviewadapte
 
     @Override
     public void onBindViewHolder(Gifgridviewadapter.ViewHolder holder, int position) {
-        Glide.with(context).load(mDataset.get(position).getThumbnailGif())
-                .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
-                .error(R.mipmap.ic_launcher)
-                .listener(new RequestListener<Drawable>() {
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        Log.e("Execption" , "on glide is"+model.toString());
-                        return false;
-                    }
+        Gifdata gifData = mDataset.get(position);
 
-                    @Override
-                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                        return false;
-                    }
-                }).into(holder.gifitemgg);
+        boolean showTextOnly = session.showTextInsteadOfThumbnail();
 
+        if (showTextOnly) {
+            holder.gifitemgg.setVisibility(View.GONE);
+            holder.gifText.setVisibility(View.VISIBLE);
+            holder.gifText.setText(gifData.getMultilineText()); // Display text instead of GIF
+        } else {
+            holder.gifitemgg.setVisibility(View.VISIBLE);
+            holder.gifText.setVisibility(View.GONE);
+
+            Glide.with(context).load(gifData.getThumbnailGif())
+                    .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+                    .error(R.mipmap.ic_launcher)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            Log.e("Exception", "Glide load failed for: " + model.toString());
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            return false;
+                        }
+                    }).into(holder.gifitemgg);
+        }
     }
 
     @Override
